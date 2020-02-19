f465b1e483d4381595e33a80d6a8923f4e4abe9b
Jackson <jacksonzamo@gmail.com>
2020-02-18 18:11:39 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+const express = require('express');
+const package = require('./package.json');
+const cron = require('cron').CronJob;
+const fs = require('fs');
+const path = require('path');
+const { exec } = require("child_process");
+const moment = require('moment');
+
+const homedir = require('os').homedir();
+const datadir = homedir
+const backupsdir = path.join(homedir, 'backups')
+
+const config = require(path.join(datadir, 'config.json'))
+
+const app = express();
+
+
+const valString = config.db.host + ':' + config.db.port + ':' + config.db.database + ':' + config.db.username + ':' + config.db.password
+fs.writeFile(path.join(homedir, '.pgpass'), valString, (err)=>{});
+exec('chmod 0600 ' + path.join(homedir, '.pgpass'));
+
+//Job setup
+const job = new cron('0 * * * *', function(){
+    downloadBackup()
+}, null, true, "America/Boise")
+job.start();
+
+app.get('/about', function(req, res){
+    res.send({status:true,version:package.version})
+})
+
+
+async function downloadBackup() {
+    const exists = fs.existsSync(backupsdir)
+    if (!exists) {
+        fs.mkdirSync(backupsdir);
+    }
+    const timestamp = moment()
+    const command = 'pg_dump -h ' + config.db.host + ' -p ' + config.db.port + ' -U ' + config.db.username + ' ' + config.db.database + ' > ' + path.join(backupsdir, timestamp.toISOString() + '.bak');
+    exec(command, (err, stdout, stderr) => {
+        console.log(stderr)
+        console.log('Backup for ' + timestamp.format('MMMM Do YYYY, h:mm:ss a') + ' complete.')
+    })
+}
+
+console.log('Verifying settings - creating test backup.')
+downloadBackup()
+app.listen(5101);
+console.log('Live at port 5101.')
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Step 1/4 - Installing required software..."
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+RELEASE=$(lsb_release -cs)
+echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg main | tee  /etc/apt/sources.list.d/pgdg.list
+apt update
+apt -y install postgresql-client-11 nodejs
+
+echo "Step 2/4 - Creating user..."
+groupadd monitordevice
+useradd -g monitordevice -m monitordevice
+
+echo "Step 3/4 - Copying files..."
+mkdir /home/monitordevice/backups
+cp config/config.json /home/monitordevice/config.json
+cp -R ./ /home/monitordevice/bin
+chown -R monitordevice:monitordevice ~/
+chmod +x /home/monitordevice/bin/index.js
+cp config/config.service /etc/systemd/system/monitordevice.service
+
+echo "Step 4/4 - Finalizing..."
+cd /home/monitordevice/bin 
+npm install
+systemctl enable monitordevice.service
+systemctl restart monitordevice.service
