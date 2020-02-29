a167d9c1d736a31510a6639ef9f33259b72efc11
Jackson Zamorano <jacksonzamo@gmail.com>
2020-02-28 22:58:14 -0700

    Updated scripts

--- update.sh
+++ update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+chown -R monitordevice:monitordevice ./
+chmod -R 766 ./
+su monitordevice
 git reset --hard
 git fetch origin master
 git pull origin master
 chmod +x ./install.sh
 chmod +x ./update.sh
 chmod +x ./index.js
+chown -R monitordevice:monitordevice ./
+chmod -R 766 ./
 npm install
 systemctl restart monitordevice.service
