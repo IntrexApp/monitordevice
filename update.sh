fbd6dc311b89b1501a23ef03c3a4542e526a06f8
Jackson <jacksonzamo@gmail.com>
2020-07-20 18:47:32 -0600

    Updated update script

--- update.sh
+++ update.sh
@@ -5,7 +5,7 @@ git pull origin master
 chmod +x ./install.sh
 chmod +x ./update.sh
 chmod +x ./index.js
-chown -R monitordevice:monitordevice /home/monitordevice/bin
-chmod -R +777 /home/monitordevice
+chown -R pgclone:pgclone /home/pgclone/bin
+chmod -R +777 /home/pgclone
 npm install
-systemctl restart monitordevice.service
+systemctl restart pgclone.service
